b86cdd8a2f44b9ba16f55ddabd4f2aa01c54acc2
Tchasinga jacques <tchasingajacques@gmail.com>
2023-09-26 15:45:35 -0700

    new app added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-   get "/about", to: "about#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-   get "/",  to: "main#index" 
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
