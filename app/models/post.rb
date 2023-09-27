b2eb926fcc96be151793de94be9e4bfae5f5f98b
Tchasinga jacques <tchasingajacques@gmail.com>
2023-09-27 15:36:09 -0700

    erors fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
-  has_many : likes
+  has_many :likes
 
-  after_save :update_user_posts_counter
+  after_save :update_user_posts_counter 
 
   def update_user_posts_counter
     author.update(posts_count: author.posts.count)
