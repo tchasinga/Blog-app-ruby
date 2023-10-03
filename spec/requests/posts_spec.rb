42691aa23716e0c2fba08ddbb196894730fc5e14
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-03 14:59:22 -0700

    new commit

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before :each do
+      get '/users/:user_id/posts'
+    end
+
+    it 'checks the response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'checks if the index template is rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'checks if the response body includes the "Posts of the user" placeholder text' do
+      expect(response.body).to include('Posts of the user')
+    end
+  end
+  describe 'GET /show' do
+    before :each do
+      get '/users/:user_id/posts/:id'
+    end
+
+    it 'checks the response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'checks if the show template is rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'checks if the response body includes the "Post with the user id" placeholder text' do
+      expect(response.body).to include('Post with the user id')
+    end
   end
 end
