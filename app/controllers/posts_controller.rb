ea1c3c0f609c32dfe43c48db4cf9924a52bdd8c6
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-05 22:59:43 -0700

    new commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,26 @@ class PostsController < ApplicationController
 
   def show
     @posts = [Post.find(params[:id])]
+    @comment = Comment.new
+    @like = Like.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,12 @@ class Post < ApplicationRecord
   validates :likesCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   after_create :update_user_posts_counter
+  after_initialize :set_default
+
+  def set_default
+    self.likesCounter ||= 0
+    self.commentsCounter ||= 0
+  end
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
