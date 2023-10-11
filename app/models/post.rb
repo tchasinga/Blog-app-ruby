18ca565e9bcecc8e48d4e2ce0ebbdc78536c57f0
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-10 17:36:55 -0700

    new pushing

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,17 +3,17 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :commentsCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-  validates :likesCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  # validates :title, presence: true, length: { maximum: 250 }
+  # validates :commentsCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  # validates :likesCounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
-  after_create :update_user_posts_counter
-  after_initialize :set_default
+  # after_create :update_user_posts_counter
+  # after_initialize :set_default
 
-  def set_default
-    self.likesCounter ||= 0
-    self.commentsCounter ||= 0
-  end
+  # def set_default
+  #   self.likesCounter ||= 0
+  #   self.commentsCounter ||= 0
+  # end
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
